5dd9d20b8c730b5ba41b565e36262654e6728b0e
lindsaykh <lindsay.hussey@gmail.com>
2021-03-16 23:30:35 -0400

    final upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # javascript-challenge
 UNC CH BC HW
+
+The UFO-level-1 folder contains a static folder and an index.html. Within the static folder there exists js, css,
+and image folders. The css and image folders contain components referenced by index.html (formatting code and images).
+The js folder contains two js files: data and app.js. The data file contains data reference by the app and posted on
+the html site depending on input values. app.js contains javascript code that uses D3 to select objects on the index.html
+page and user input to filter data values and populate these values onto the html page. Directions for use are given below.
+
+Open the index.html file and type a date (ranging from 01/1/2010 to 01/13/2010). After clicking on the button below
+the input box a list of UFO sightings with location, comments, and other information will populate the webpage if a sighting
+exists in the dataset. 
+
+Comments: Future improvements could/will include an error message or notification to the user if the input date is 
+of an incorrect format or not within the dataset (not in this current version).
+
+References: 
+https://github.com/goldenb85
+https://stackoverflow.com/questions/13291661/check-if-a-date-within-in-range
--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,40 +1,46 @@
-// from data.js
+// define data table from data.js 
 var tableData = data;
+console.log("nothing is here")
 
-//Automatic Table and Date Search
-// Using the UFO dataset provided in the form of an array of JavaScript objects, 
-// write code that appends a table to your web page and then adds new rows of data 
-//for each UFO sighting.
+// define the button from id in index.html 
+var button = d3.select("#filter-btn");
 
-//Make sure you have a column for date/time, city, state, country, shape, 
-//and comment at the very least.
+// define action after button (Filter Sightings) is clicked 
+button.on("click", function() {
 
-//Use a date form in your HTML document and write JavaScript code that will listen for events 
-//and search through the date/time column to find rows that match user input.
+  // define/select the input element from the index.html input id datetime
+  var inputElement = d3.select("#datetime");
 
-// Get a reference to the table body
-var tbody = d3.select("tbody");
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
 
-// reference the table
-var table = d3.select("table");
+  // log the values
+  console.log(`the inputValue is ${inputValue}`);
+  // console.log(`the corresponding data for the input is ${tableData}`);
 
-// from html grab table class "table-striped"
-table.attr("class", "table table-striped");
+    // Use D3 to select the table body
+  var tbody = d3.select("tbody");
 
-//  Use d3 to update each cell's text with UFO Sighting Values (date/time, city, state, country, shape, and comment)
-data.forEach(function(UFOReport) {
-    console.log(UFOReport);
-    var row = tbody.append("tr");
-    Object.entries(UFOReport).forEach(function([key, value]) {
-//     console.log(key, value);
+  // Use D3 to select the table
+  var table = d3.select("table");
 
-//    Append a cell to the row for each value in the UFO report object
-    var cell = row.append("td");
-    cell.text(value);
+  //remove any existing data from the table so as not to just append new values to previous values 
+  tbody.html("");
+
+  // Use D3 to set the table class to `table table-striped`
+  table.attr("class", "table table-striped");
+
+  // filter the data based on the input 
+   var filteredData = tableData.filter(ufoSighting => (ufoSighting.datetime === inputValue));
+  
+   // Use d3 to append filtered data per UFO sighting value 
+  filteredData.forEach(function(ufoSighting){
+    console.log("something is here");
+    var row =tbody.append("tr");
+    //return an array of key,value entries
+     Object.entries(ufoSighting).forEach(([key,value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
   });
-});
-
-// Getting a reference to the input element on the page with the class property set to 'form-control'
-var inputField = d3.select("form-control");
-
-// Select Filter Table button
+  });
